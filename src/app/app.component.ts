28e94e7f18da1e065ad496736b06a382dc36e563
alexey-belov1 <veshousis1@gmail.com>
2020-09-23 19:08:39 +0500

    13. Как работает Event Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Dynamic title';
   number = 42;
   arr = [1, 2, 3];
   obj = {a: 1, b: {c: 2} };
-  img = 'https://lh3.googleusercontent.com/HDhEYpJwOs5zFq1ckigVoKn0NYZxdLz3NzyIMt94jDlfgbbvLkYjHdBTC6CJO_dH82oc';
+  inputValue = '';
+  /*img = 'https://lh3.googleusercontent.com/HDhEYpJwOs5zFq1ckigVoKn0NYZxdLz3NzyIMt94jDlfgbbvLkYjHdBTC6CJO_dH82oc';*/
 
   constructor() {
-    setTimeout(() => {
+    /*setTimeout(() => {
       console.log('Timeout is over');
       this.img = 'https://icon-library.com/images/react-icon/react-icon-14.jpg';
-    }, 5000);
+    }, 5000);*/
+  }
+
+  onInput(event: KeyboardEvent): void {
+    this.inputValue = (<HTMLInputElement> event.target).value;
+  }
+
+  onBlur(str: string): void {
+    this.inputValue = str;
+  }
+
+  onClick(): void {
+    console.log('Click!');
   }
 }
