1120c922b553d3885e61f21ee94175c3abedc7cc
alexey-belov1 <veshousis1@gmail.com>
2020-09-23 19:47:13 +0500

    14. 2 Way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,5 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'Dynamic title';
-  number = 42;
-  arr = [1, 2, 3];
-  obj = {a: 1, b: {c: 2} };
-  inputValue = '';
-  /*img = 'https://lh3.googleusercontent.com/HDhEYpJwOs5zFq1ckigVoKn0NYZxdLz3NzyIMt94jDlfgbbvLkYjHdBTC6CJO_dH82oc';*/
-
-  constructor() {
-    /*setTimeout(() => {
-      console.log('Timeout is over');
-      this.img = 'https://icon-library.com/images/react-icon/react-icon-14.jpg';
-    }, 5000);*/
-  }
-
-  onInput(event: KeyboardEvent): void {
-    this.inputValue = (<HTMLInputElement> event.target).value;
-  }
-
-  onBlur(str: string): void {
-    this.inputValue = str;
-  }
-
-  onClick(): void {
-    console.log('Click!');
-  }
+  title = 'Initial';
 }
