51617a78a041a0f77dcb72b0bdb39057c7657e7c
alexey-belov1 <veshousis1@gmail.com>
2020-09-23 17:59:12 +0500

    12. Что такое bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,12 @@ export class AppComponent {
   number = 42;
   arr = [1, 2, 3];
   obj = {a: 1, b: {c: 2} };
+  img = 'https://lh3.googleusercontent.com/HDhEYpJwOs5zFq1ckigVoKn0NYZxdLz3NzyIMt94jDlfgbbvLkYjHdBTC6CJO_dH82oc';
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Timeout is over');
+      this.img = 'https://icon-library.com/images/react-icon/react-icon-14.jpg';
+    }, 5000);
+  }
 }
