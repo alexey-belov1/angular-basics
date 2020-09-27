6e5d634462cc4994f33a29a78ac40e76b565a80b
alexey-belov1 <veshousis1@gmail.com>
2020-09-27 16:23:28 +0600

    27. Жизненный цикл (lifecycle hooks)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,4 +22,9 @@ export class AppComponent {
     this.posts.unshift(post);
     // console.log('Post: ', post);
   }
+
+  removePost(id: number): void {
+    console.log('Id to remove', id);
+    this.posts = this.posts.filter(p => p.id !== id);
+  }
 }
